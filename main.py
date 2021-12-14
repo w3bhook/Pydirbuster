272e156c9a8bc5ec2cb5d9666292e2cdb9eed983
Jan Dorian Poczekaj <66368602+w3bhook@users.noreply.github.com>
2021-12-14 17:07:35 +0000

    Improved code - w3bhook
    Full code from video, including license at top, new wordlist in /wordlists/ . fixed remaining errors, set new wordlist file in main.py

--- main.py
+++ main.py
@@ -16,80 +16,75 @@ from requests import get, exceptions
 from sys import argv
 from art import tprint
 
-version = "1.0.0"
-layer2 = list()
+v = '1.0.0' # sets the version of the program
+layer2 = list() # creates a variable called layer2 and makes it an empty array
 
-def main(url, wordlist = ".\\wordlists\\pydirbuster_wordlist_small.txt"):
-	with open(wordlist) as words:
+def main(url, wordlist = "wordlists/pydirbuster_wordlist_small.txt"): # defines the main function and adds a default wordlist to the parameters
+	with open(wordlist) as words: # opens the wordlist file
 
-			words = (words.read()).splitlines()
+		words = (words.read()).splitlines() # reads all the lines one by one in the wordlist
 
-			print(f"WORDLIST: {wordlist}")
-			print(f"WORDLIST LINES: {len(words)}")
-			print(f"URL: {url}")
-			print("---------------]\n\nBusting Directories...\n")
+		print(f"WORDLIST: {wordlist}")										# tells you what wordlist you are using
+		print(f"WORDLIST LINES: {len(words)}")								# tells you how many words/lines are in the wordlist
+		print(f"URL: {url}")												# returns the current victim url
+		print("-----------------------------]\n\nBusting Directories...\n")	# aesthetics
 
-			for count in words:
-				try:
+		for count in words: # iterates through all the words
+			try: # catches any errors
+				res = get(url=url+count) # sends a response to the given url with the word
+				if "200" in str(res): # checks if the url is valid
+					print(f"{url}{count}") # if the url is valid, it prints the url onto the screen
+					layer2.append(url+count) # appends the valid url to the layer2 array
 
-					res = get(url=url+count)
+				res = get(url=url+count+".html") # ", more checks
+				if "200" in str(res):
+					print(f"{url}{count}.html")
+					layer2.append(url+count)
+
+				res = get(url=url+count+"/") # ", more checks
+				if "200" in str(res):
+					print(f"{url}{count}/")
+					layer2.append(url+count)
+
+			except exceptions.ConnectionError: # if an error is in the above code, where you can not access the url, this fallback code will run
+				print(f"Unable to reach given url: {url}") # tells the user which url can't be accessed
+				quit() # quits the program
+
+		for count in words:
+			try:
+
+				for l2 in layer2:
+
+					res = get(url=l2+"/") # TypeError: can only concatenate list (not "str") to list
 					if "200" in str(res):
-						print(f"{url}{count}")
-						layer2.append(url+count)
+						print(f"{url}{count}/{count}")
 
-					res = get(url=url+count+".html")
+					res = get(url=l2+"/"+count+".html")
 					if "200" in str(res):
-						print(f"{url}{count}.html")
-						layer2.append(url+count)
+						print(f"{url}{count}/{count}.html")
 
-					res = get(url=url+count+"/")
-					if "200" in str(res):
-						print(f"{url}{count}/")
-						layer2.append(url+count)
-
-				except exceptions.ConnectionError:
-					print(f"Unable to reach given url: {url}")
-					quit()
-
-			for count in words:
-				try:
-
-					res = get(url=layer2+"/"+count)
-					if "200" in str(res):
-						print(f"{url}/{count}")
-
-					res = get(url+layer2+"/"+count+".html")
-					if "200" in str(res):
-						print(f"{url}/{count}.html")
-
-					res = get(url+layer2+"/"+count+"/")
-					if "200" in str(res):
-						print(f"{url}/{count}/")
-
-				except exceptions.ConnectionError:
-					print(f"Unable to reach given url: {url}")
-					quit()
-
-			print("\nFinished scanning. All URL's returned back have returned HTML Response Code 200")
+			except exceptions.ConnectionError: # if an error is in the above code, where you can not access the url, this fallback code will run
+				print(f"Unable to reach given url: {url}") # tells the user which url can't be accessed
+				quit() # quits the program
 
 
-if __name__ == "__main__":
+		print("\nFinished scanning. All URL's returned back have given a HTML Response Code of 200") # aesthetic
 
-	tprint(f"PyDirbuster              {version}")
-	print("-------------------------------------------------------------------------------------------------------\n\n")
-	print("[---------------")
+if __name__ == '__main__':
 
+	tprint(f"PyDirbuster         {v}")
+	print("-------------------------------------------------------------------------\n\n")
+	print("[-----------------------------")
 
+	if len(argv) == 1: # checks if user has inputted anything as an argument
+		print("usage: python main.py <url> [./path/to/wordlist.txt]")
+		quit()
 
-	if len(argv) == 1:
-		print("usage: python3 main.py <url> [./path/to/wordlist.txt]")
-		print("---------------]")
-
-	elif len(argv) == 2:
+	elif len(argv) == 2: # checks if user has added a url but not wordlist
 
 		if "http" not in argv[1]:
 			print("Not a valid url. please make sure you have the `http://` or `https://` prefix in it.")
-			print("---------------]")
+			print("-----------------------------]")
 			quit()
 
 		if "/" not in (argv[1])[-1]:
@@ -97,17 +92,22 @@ if __name__ == "__main__":
 
 		main(argv[1])
 
-	elif len(argv) == 3:
+	elif len(argv) == 3: # checks if user has added a url and wordlist
 
 		if "http" not in argv[1]:
 			print("Not a valid url. please make sure you have the `http://` or `https://` prefix in it.")
-			print("---------------]")
+			print("-----------------------------]")
 			quit()
-			
+
 		if "/" not in (argv[1])[-1]:
 			argv[1] = argv[1] + "/"
 
 		main(argv[1], argv[2])
 
 	else:
-		print("usage: python3 main.py <url> [./path/to/wordlist.txt]")
+
+		print("usage: python main.py <url> [./path/to/wordlist.txt]")
+
+###								###
+###			END OF PROGRAM 		###
+###								###
